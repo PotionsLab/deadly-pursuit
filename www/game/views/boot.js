3d091b5e07c5900176ef77fb8de026914ac8ccd3
Slava <vyacheslav.gorodovych@edvantis.com>
2017-11-14 20:33:54 +0100

    #4: Add road with animation and basic car movements

--- www/game/views/boot.js
+++ www/game/views/boot.js
@@ -2,7 +2,14 @@ import Phaser from 'phaser'
 import WebFont from 'webfontloader'
 
 export default class extends Phaser.State {
-  init () {}
+  
+  init () {
+    this.SPRITE_URL = './www/assets/sprites/';
+  }
+
+  getResourcePath(name) {
+    return `${this.SPRITE_URL}${name}`;
+  } 
 
   preload () {
 
@@ -14,9 +21,13 @@ export default class extends Phaser.State {
     //   active: this.fontsLoaded
     // });
 
-    this.load.image('menuBg', './www/assets/sprites/menu_view_bg.png');
-    this.load.spritesheet('mainPlayBtn', './www/assets/sprites/play_button.png', 223, 55);
-
+    this.load.image('menuBg', this.getResourcePath('menu_view_bg.png'));
+    this.load.spritesheet('mainPlayBtn', this.getResourcePath('play_button.png'), 223, 55);
+    this.load.image('road', this.getResourcePath('road_daylight_bg.png'));
+    this.load.image('redCar', this.getResourcePath('car_top_red.png'));
+    this.load.image('dashboardBg', this.getResourcePath('dashboard_bg.png'));
+    this.load.image('fuelBar', this.getResourcePath('fuel_bar.png'));
+    
     this.fontsLoaded();
   }
 
--- www/game/views/game.js
+++ www/game/views/game.js
@@ -4,9 +4,30 @@ import Phaser from 'phaser'
 export default class extends Phaser.State {
   init () {}
   
-  preload () {}
+  preload () {
+    this.road = this.add.tileSprite(0, 0, 270, 392, 'road');
+    this.redCar = this.add.sprite(game.world.width - 150, game.world.height - 150, 'redCar');
+    this.dashboardBg = this.add.image(0, game.world.height - 98, 'dashboardBg');
+    this.fuelBar = this.add.image(10, game.world.height - 70, 'fuelBar');
+  }
 
-  create () {}
+  create () {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-  update () {}
+    this.redCar.anchor.setTo(0, 0);
+    game.physics.enable(this.redCar, Phaser.Physics.ARCADE);
+
+    this.cursors = game.input.keyboard.createCursorKeys();
+  }
+
+  update () {
+    this.road.tilePosition.y += 2;
+    if (this.cursors.left.isDown) {
+        this.redCar.body.velocity.x = -200;
+    } else if (this.cursors.right.isDown) {
+        this.redCar.body.velocity.x = 200;
+    } else {
+      this.redCar.body.velocity.x = 0;
+    }
+  }
 }
