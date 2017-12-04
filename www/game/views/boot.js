210869e15a03e0301b5ba4aa178853fae856ccc3
Archon <archon@archons.info>
2017-12-04 23:06:32 +0100

    Added on key down effect to buttons

--- www/game/views/boot.js
+++ www/game/views/boot.js
@@ -22,16 +22,16 @@ export default class extends Phaser.State {
     // });
 
     this.load.image('menuBg', this.getResourcePath('menu_view_bg.png'));
-    this.load.spritesheet('mainPlayBtn', this.getResourcePath('play_button.png'), 223, 55);
+    this.load.spritesheet('mainPlayBtn', this.getResourcePath('play_button.png'), 222, 56, 2);
     this.load.image('road', this.getResourcePath('road_daylight_bg.png'));
     this.load.image('redCar', this.getResourcePath('car_top_red.png'));
     this.load.image('dashboardBg', this.getResourcePath('dashboard_bg.png'));
     this.load.image('fuelBar', this.getResourcePath('fuel_bar.png'));
 
-    this.load.spritesheet('exitBtn', this.getResourcePath('exit_button.png'), 49, 60);
-    this.load.spritesheet('garageBtn', this.getResourcePath('garage_button.png'), 49, 60);
-    this.load.spritesheet('rankBtn', this.getResourcePath('rank_button.png'), 49, 60);
-    this.load.spritesheet('settingsBtn', this.getResourcePath('settings_button.png'), 49, 60);
+    this.load.spritesheet('exitBtn', this.getResourcePath('exit_button.png'), 48, 60, 2);
+    this.load.spritesheet('garageBtn', this.getResourcePath('garage_button.png'), 48, 60, 2);
+    this.load.spritesheet('rankBtn', this.getResourcePath('rank_button.png'), 48, 60, 2);
+    this.load.spritesheet('settingsBtn', this.getResourcePath('settings_button.png'), 48, 60, 2);
     
     this.fontsLoaded();
   }
--- www/game/views/splash.js
+++ www/game/views/splash.js
@@ -7,9 +7,9 @@ export default class extends Phaser.State {
     this.menuBg = this.add.image(0, 0, 'menuBg');
     //TODO: Button click animation not working, maybe the bad sprite sizing
     this.mainPlayBtn = this.add.button(25, game.world.height - 130, 'mainPlayBtn', this.startGame, this, 0, 0, 1);
-    this.rankBtn = this.add.button(25, game.world.height - 65, 'rankBtn');
-    this.garageBtn = this.add.button(85, game.world.height - 65, 'garageBtn');
-    this.settingsBtn = this.add.button(145, game.world.height - 65, 'settingsBtn');
+    this.rankBtn = this.add.button(25, game.world.height - 65, 'rankBtn', () => {}, this, 0, 0, 1);
+    this.garageBtn = this.add.button(85, game.world.height - 65, 'garageBtn', () => {}, this, 0, 0, 1);
+    this.settingsBtn = this.add.button(145, game.world.height - 65, 'settingsBtn', () => {}, this, 0, 0, 1);
     this.exitBtn = this.add.button(205, game.world.height - 65, 'exitBtn', this.exitBtn, this, 0, 0, 1);
   }
 
