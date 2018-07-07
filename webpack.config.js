05a06413e8676102222a0c6880f2818be96cb7b2
Archon <archon@archons.info>
2018-07-07 19:08:54 +0200

    AVA to unit tests intelled! 🎉

--- webpack.config.js
+++ webpack.config.js
@@ -41,10 +41,24 @@ let webpackConfig = (env) => {
     ],
     module: {
       rules: [
-        { test: /\.js$/, use: ['babel-loader'], include: path.join(__dirname, 'src') },
-        { test: /pixi\.js/, use: ['expose-loader?PIXI'] },
-        { test: /phaser-split\.js$/, use: ['expose-loader?Phaser'] },
-        { test: /p2\.js/, use: ['expose-loader?p2'] }
+        {
+          test: /\.js$/,
+          use: ['babel-loader'],
+          include: path.join(__dirname, 'src/**/*'),
+          exclude: path.join(__dirname, 'src/**/*.test.js')
+        },
+        {
+          test: /pixi\.js/,
+          use: ['expose-loader?PIXI']
+        },
+        {
+          test: /phaser-split\.js$/,
+          use: ['expose-loader?Phaser']
+        },
+        {
+          test: /p2\.js/,
+          use: ['expose-loader?p2']
+        }
       ]
     },
     node: {
